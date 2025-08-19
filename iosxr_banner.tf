d8fec4867cc658853a3cc73823d2e0d2408370c7
Praveen Matanam <praveen.matanam26@gmail.com>
2025-08-19 14:57:34 +0530

    Update iosxr_banner.tf

--- iosxr_banner.tf
+++ iosxr_banner.tf
@@ -1,15 +1,26 @@
-resource "iosxr_banner" "login" {
-  for_each = {
-    for device in local.devices : device.name => device
-    if try(local.device_config[device.name].banner.login.lines, null) != null ||
-       try(local.defaults.iosxr.configuration.banner.login.lines, null) != null
-  }
+# Login Banner Resources
+resource "iosxr_banner" "login_banner" {
+  for_each = { for device in local.devices : "${device.name}-login" => device if try(local.device_config[device.name].banner.login, local.defaults.iosxr.configuration.banner.login, null) != null }
+  device   = each.value.name
 
-  device      = each.value.name
   banner_type = "login"
-  line        = join("\n", try(
-    local.device_config[each.value.name].banner.login.lines,
-    local.defaults.iosxr.configuration.banner.login.lines,
-    []
-  ))
+  line = try(local.device_config[each.value.name].banner.login, local.defaults.iosxr.configuration.banner.login)
+}
+
+# MOTD Banner Resources
+resource "iosxr_banner" "motd_banner" {
+  for_each = { for device in local.devices : "${device.name}-motd" => device if try(local.device_config[device.name].banner.motd, local.defaults.iosxr.configuration.banner.motd, null) != null }
+  device   = each.value.name
+
+  banner_type = "motd"
+  line = try(local.device_config[each.value.name].banner.motd, local.defaults.iosxr.configuration.banner.motd)
+}
+
+# EXEC Banner Resources
+resource "iosxr_banner" "exec_banner" {
+  for_each = { for device in local.devices : "${device.name}-exec" => device if try(local.device_config[device.name].banner.exec, local.defaults.iosxr.configuration.banner.exec, null) != null }
+  device   = each.value.name
+
+  banner_type = "exec"
+  line = try(local.device_config[each.value.name].banner.exec, local.defaults.iosxr.configuration.banner.exec)
 }
