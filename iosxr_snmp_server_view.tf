d20d1d7468735347780889b4e67f13e7746176db
rpatraga <>
2025-09-08 12:33:30 +0530

    implemented iosxr_snmp_server_view feature.

---
+++ iosxr_snmp_server_view.tf
@@ -0,0 +1,28 @@
+locals {
+  device_snmp_server_view_configs = flatten([
+    for device in local.devices : [
+      for view in try(local.device_config[device.name].snmp_server_views, local.defaults.iosxr.configuration.snmp_server_views, []) : {
+        device_name = device.name
+        key         = "${device.name}-snmp-server-view-${view.view_name}"
+
+        view_name = try(view.view_name, null)
+
+        mib_view_families = [
+          for family in try(view.mib_view_families, []) : {
+            name     = try(family.name, null)
+            excluded = try(family.excluded, null)
+            included = try(family.included, null)
+          }
+        ]
+      }
+    ] if try(local.device_config[device.name].snmp_server_views, local.defaults.iosxr.configuration.snmp_server_views, null) != null
+  ])
+}
+
+resource "iosxr_snmp_server_view" "snmp_server_view" {
+  for_each = { for view_config in local.device_snmp_server_view_configs : view_config.key => view_config }
+  device   = each.value.device_name
+
+  view_name         = each.value.view_name
+  mib_view_families = each.value.mib_view_families
+}
