1c1f34cd6005e0bd8fb30dffea94db0aa652f6c3
rocrowe <rocrowe@cisco.com>
2025-09-30 09:39:01 -0400

    refactor(iosxr_domain):  allow domain defaults
    - Updated for_each condition to include default domain check
    - Removed unnecessary default values for lists in domains and name_servers

--- iosxr_domain.tf
+++ iosxr_domain.tf
@@ -1,12 +1,12 @@
 resource "iosxr_domain" "domain" {
-  for_each = { for device in local.devices : device.name => device if try(local.device_config[device.name].domain, null) != null }
+  for_each = { for device in local.devices : device.name => device if try(local.device_config[device.name].domain, null) != null || try(local.defaults.iosxr.configuration.domain, null) != null }
   device   = each.value.name
 
   name                    = try(local.device_config[each.value.name].domain.name, local.defaults.iosxr.configuration.domain.name, null)
-  domains                 = try(length(local.device_config[each.value.name].domain.domains) == 0, true) ? null : try(local.device_config[each.value.name].domain.domains, local.defaults.iosxr.configuration.domain.domains, null)
+  domains                 = try(length(local.device_config[each.value.name].domain.domains) == 0, true) ? null : try(local.device_config[each.value.name].domain.domains, null)
   lookup_disable          = try(local.device_config[each.value.name].domain.lookup_disable, local.defaults.iosxr.configuration.domain.lookup_disable, null)
   lookup_source_interface = try(local.device_config[each.value.name].domain.lookup_source_interface, local.defaults.iosxr.configuration.domain.lookup_source_interface, null)
-  name_servers            = try(length(local.device_config[each.value.name].domain.name_servers) == 0, true) ? null : try(local.device_config[each.value.name].domain.name_servers, local.defaults.iosxr.configuration.domain.name_servers, null)
+  name_servers            = try(length(local.device_config[each.value.name].domain.name_servers) == 0, true) ? null : try(local.device_config[each.value.name].domain.name_servers, null)
   ipv4_hosts              = try(length(local.device_config[each.value.name].domain.ipv4_hosts) == 0, true) ? null : try(local.device_config[each.value.name].domain.ipv4_hosts, null)
   ipv6_hosts              = try(length(local.device_config[each.value.name].domain.ipv6_hosts) == 0, true) ? null : try(local.device_config[each.value.name].domain.ipv6_hosts, null)
   multicast               = try(local.device_config[each.value.name].domain.multicast, local.defaults.iosxr.configuration.domain.multicast, null)
