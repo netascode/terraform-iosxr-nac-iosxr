d079d6cf5672337317856630bed78440555f366e
rasimh542 <rasimh542@cisco.com>
2025-10-03 00:24:02 +0200

    default main and system nac

--- examples/system/main.tf
+++ examples/system/main.tf
@@ -1,6 +1,5 @@
 module "iosxr" {
-  #source     = "netascode/nac-iosxr/iosxr"
-  #version    = ">= 0.1.0"
-  source     = "../.."
+  source     = "netascode/nac-iosxr/iosxr"
+  version    = ">= 0.1.0"
   yaml_files = ["system.nac.yaml"]
 }
