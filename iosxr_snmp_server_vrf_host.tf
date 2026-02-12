37e3a15b949112cb32ab979fb34d01945e0d9914
rpatraga <rpatraga@cisco.com>
2026-02-12 09:54:07 +0530

    Added missing fields.

--- iosxr_snmp_server_vrf_host.tf
+++ iosxr_snmp_server_vrf_host.tf
@@ -9,11 +9,15 @@ locals {
         delete_mode = try(host.delete_mode, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.delete_mode, null)
         traps_unencrypted_strings = try(length(host.traps_unencrypted_strings) == 0, true) ? null : [for trap in host.traps_unencrypted_strings : {
           community_string          = try(trap.community_string, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.traps_unencrypted_strings.community_string, null)
+          udp_port                  = try(trap.udp_port, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.traps_unencrypted_strings.udp_port, null)
+          version_v2c               = try(trap.version_v2c, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.traps_unencrypted_strings.version_v2c, null)
           version_v3_security_level = try(trap.version_v3_security_level, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.traps_unencrypted_strings.version_v3_security_level, null)
           }
         ]
         informs_unencrypted_strings = try(length(host.informs_unencrypted_strings) == 0, true) ? null : [for inform in host.informs_unencrypted_strings : {
           community_string          = try(inform.community_string, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.informs_unencrypted_strings.community_string, null)
+          udp_port                  = try(inform.udp_port, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.informs_unencrypted_strings.udp_port, null)
+          version_v2c               = try(inform.version_v2c, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.informs_unencrypted_strings.version_v2c, null)
           version_v3_security_level = try(inform.version_v3_security_level, local.defaults.iosxr.devices.configuration.snmp_server_vrf_hosts.informs_unencrypted_strings.version_v3_security_level, null)
           }
         ]
